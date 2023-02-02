80637b5f695ae0653adaa0332042a625c19384fe
Alireza <alirezaaa1194@gmail.com>
2023-02-03 02:43:25 +0330

    add auto check option

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 function menuBtnFunction(menuBtn) {
     menuBtn.classList.toggle("active");
 }
+let counter=0;
+setInterval(function(){
+    counter++
+if(counter==10){
+    counter=1;
+}
+document.querySelector('.radio'+counter).checked=true;
+},6000)
