9df08d235859bc1e164303c7b92c4c2658e51b58
Alireza Nouri <alirezaaa1194@gmail.com>
2023-02-05 00:46:57 +0330

    Fix bug of scroll 2

--- app.js
+++ app.js
@@ -1,6 +1,38 @@
+  let radio1=document.querySelector('.radio1');
+  let radio2=document.querySelector('.radio2');
+  let radio3=document.querySelector('.radio3');
+  let radio4=document.querySelector('.radio4');
+  let radio5=document.querySelector('.radio5');
+  let radio6=document.querySelector('.radio6');
+  let radio7=document.querySelector('.radio7');
+  let radio8=document.querySelector('.radio8');
+  let radio9=document.querySelector('.radio9');
 function menuBtnFunction(menuBtn) {
   menuBtn.classList.toggle("active");
 }
+if(radio1.checked==true){
+    counter=1;
+  }
+   if (radio2.checked == true) {
+     counter = 2;
+   }
+    if (radio3.checked == true) {
+      counter = 3;
+    }
+     if (radio4.checked == true) {
+       counter = 4;
+     }
+      if (radio5.checked == true) {
+        counter = 5;
+      } if (radio6.checked == true) {
+         counter = 6;
+       } if (radio7.checked == true) {
+          counter = 7;
+        } if (radio8.checked == true) {
+           counter = 8;
+         } if (radio9.checked == true) {
+            counter = 9;
+          }
 let counter = 0;
 setInterval(function () {
   counter++;
@@ -15,4 +47,27 @@ setInterval(function () {
   }
   //   document.getElementById("radif").scrollLeft = counter * 80-80;
   document.querySelector(".radio" + counter).checked = true;
+if(radio1.checked==true){
+    counter=1;
+  }
+   if (radio2.checked == true) {
+     counter = 2;
+   }
+    if (radio3.checked == true) {
+      counter = 3;
+    }
+     if (radio4.checked == true) {
+       counter = 4;
+     }
+      if (radio5.checked == true) {
+        counter = 5;
+      } if (radio6.checked == true) {
+         counter = 6;
+       } if (radio7.checked == true) {
+          counter = 7;
+        } if (radio8.checked == true) {
+           counter = 8;
+         } if (radio9.checked == true) {
+            counter = 9;
+          }
 }, 6000);
