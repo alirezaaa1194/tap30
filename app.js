d212c33a007a009554cb5ce77bb9aaca046ce25c
Alireza <alirezaaa1194@gmail.com>
2023-02-04 21:53:27 +0330

    fix scroll bugs

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 function menuBtnFunction(menuBtn) {
-    menuBtn.classList.toggle("active");
+  menuBtn.classList.toggle("active");
 }
-let counter=0;
-setInterval(function(){
-    counter++
-if(counter==10){
-    counter=1;
-}
-document.querySelector('.radio'+counter).checked=true;
-},6000)
+let counter = 0;
+setInterval(function () {
+  counter++;
+  if (counter == 10) {
+    counter = 1;
+    document.getElementById("radif").scrollLeft += 640;
+  } else {
+    document.getElementById("radif").scrollLeft -= 80;
+  }
+  if (counter == 1) {
+    document.getElementById("radif").scrollLeft = 0;
+  }
+  //   document.getElementById("radif").scrollLeft = counter * 80-80;
+  document.querySelector(".radio" + counter).checked = true;
+}, 6000);
